2c757d8ff47f6a88af4b0703844ccb55e5690c91
Guilherme Ranoya <ranoya@gmail.com>
2023-10-08 13:33:30 -0300

    começando a funcionar

--- level/timeline.js
+++ level/timeline.js
@@ -115,7 +115,7 @@ fetch(arquivojson).then(response => response.json()).then((dados) => {
 
         }
 
-        tituloscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].titulofundo}; color: ${dados[i].titulofrente}'>${tituloatual}</div>`;
+        tituloscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].titulofundo}; color: ${dados[i].titulofrente}'><span style='margin-left: var(--meta-margins, 30px);'>${tituloatual}</span></div>`;
 
         i = i + contat;
 
@@ -140,7 +140,7 @@ fetch(arquivojson).then(response => response.json()).then((dados) => {
 
         }
 
-        subtituloscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].subtitulofundo}; color: ${dados[i].subtitulofrente}'>${subtituloatual}</div>`;
+        subtituloscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].subtitulofundo}; color: ${dados[i].subtitulofrente}'><span style='margin-left: var(--meta-margins, 30px);'>${subtituloatual}</span></div>`;
 
         i = i + contat;
 
@@ -166,7 +166,7 @@ fetch(arquivojson).then(response => response.json()).then((dados) => {
 
         }
 
-        topicoscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].fundo}; color: ${dados[i].frente}'>${subtituloatual}</div>`;
+        topicoscode += `<div class="track" style='display: inline-block; position: sticky; height: 30px; margin: 0; padding: 0; top: 0; left: 0; width: ${contat * 100}vw; z-index: ${200 + i}; background-color: ${dados[i].fundo}; color: ${dados[i].frente}'><span style='margin-left: var(--meta-margins, 30px);'>${subtituloatual}</span></div>`;
 
         i = i + contat;
 
