67c9d4161b9961ad70f3ef5b57433bb23ec568a1
Guilherme Ranoya <ranoya@gmail.com>
2023-10-15 15:40:25 -0300

    que trabalho para ficar transparente no início

--- timelineh/timeline.js
+++ timelineh/timeline.js
@@ -78,7 +78,9 @@ if (typeof $_GET['followbg'] != 'undefined' && $_GET['followbg'] != null && $_GE
    document.documentElement.style.setProperty('--timeline-tempo', 'transparent');
 }
 
+
 document.documentElement.style.setProperty('--valor-tempo', 'transparent');
+document.documentElement.style.setProperty('-add-transparencia', 'transparent');
 
 
 document.getElementById("next").innerHTML = `
@@ -177,8 +179,10 @@ fetch(arquivojson).then(response => response.json()).then((dados) => {
 
         if (typeof $_GET['startvisible'] != 'undefined' && $_GET['startvisible'] != null && $_GET['startvisible'] != '') {
             document.documentElement.style.setProperty('--valor-tempo', dados[0].frente);
+            document.documentElement.style.setProperty('-add-transparencia', '#00000001');
         } else {
             document.documentElement.style.setProperty('--valor-tempo', dados[0].fundo);
+            document.documentElement.style.setProperty('-add-transparencia', dados[0].fundo);
         }
         
     }
@@ -473,6 +477,7 @@ document.addEventListener("wheel", (event) => {
         document.documentElement.style.setProperty('--button-color', arrcolorbg[posicao]);
         document.documentElement.style.setProperty('--track-fg', arrcolorbg[posicao]);
         document.documentElement.style.setProperty('--track-bg', arrcolorfg[posicao]);
+        document.documentElement.style.setProperty('-add-transparencia', '#00000001');
 
         if (typeof $_GET['followbg'] != "undefined" && $_GET['followbg'] != null && $_GET['followbg'] != "") {
 
@@ -588,6 +593,7 @@ document.getElementById("frontslide").addEventListener("scroll", (event) => {
     document.documentElement.style.setProperty('--button-color', arrcolorbg[posicao]);
     document.documentElement.style.setProperty('--track-fg', arrcolorbg[posicao]);
     document.documentElement.style.setProperty('--track-bg', arrcolorfg[posicao]);
+    document.documentElement.style.setProperty('-add-transparencia', '#00000001');
 
     if (typeof $_GET['followbg'] != "undefined" && $_GET['followbg'] != null && $_GET['followbg'] != "") {
 
