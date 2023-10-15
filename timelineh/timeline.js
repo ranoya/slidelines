acd3068594c6ed1bfd67a73cbf7595d23b347328
Guilherme Ranoya <ranoya@gmail.com>
2023-10-15 15:18:16 -0300

    bloco inteiro com mouseover

--- timelineh/timeline.js
+++ timelineh/timeline.js
@@ -240,7 +240,7 @@ fetch(arquivojson).then(response => response.json()).then((dados) => {
             milestone = `display: block; position: absolute; z-index: 10000; width: calc(${contat * 5}vw - 30px); transform: translate(15px,0);`;
         }
 
-        anoscode += `<div class="linhadotempo" style='display: inline-block; margin: 0; padding: 0; top: 0; left: 0; width: 5vw; margin-right: ${(contat - 1) * 5}vw; z-index: ${600 + i};'><div onclick='nowgo(${i})' class='registroano' style='${milestone}'>${tituloatual}</div></div>`;
+        anoscode += `<div class="linhadotempo" onclick='nowgo(${i})' style='display: inline-block; margin: 0; padding: 0; top: 0; left: 0; width: 5vw; margin-right: ${(contat - 1) * 5}vw; z-index: ${600 + i};'><div class='registroano' style='${milestone}'>${tituloatual}</div></div>`;
 
         i = i + contat;
 
