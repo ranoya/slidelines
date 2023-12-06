92c11e32b5e9fb7a47af995cc4d47546a1a4c840
Guilherme Ranoya <ranoya@gmail.com>
2023-12-06 19:21:52 -0300

    mais movimento e separação

--- viewporth/timeline.js
+++ viewporth/timeline.js
@@ -413,7 +413,7 @@ const putslides = function (posicao) {
             indicefixo = 0;
         }
 
-        pegatodosslides[z].style.transform=`translate(${(((document.getElementById("frontslide").scrollLeft - (indicefixo * window.innerWidth) )  / 5)) + "px"},0)`;
+        pegatodosslides[z].style.transform=`translate(${(((document.getElementById("frontslide").scrollLeft - (indicefixo * window.innerWidth) )  / 3)) + "px"},0)`;
 
     }
 
