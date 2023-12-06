7a47dfc3ed4e552a2cf76a10ebf4398fa37a6b8e
Guilherme Ranoya <ranoya@gmail.com>
2023-12-06 19:28:25 -0300

    com 2 treme muito... pq será?

--- viewporth/timeline.js
+++ viewporth/timeline.js
@@ -413,7 +413,7 @@ const putslides = function (posicao) {
             indicefixo = 0;
         }
 
-        pegatodosslides[z].style.transform=`translate(${(((document.getElementById("frontslide").scrollLeft - (indicefixo * window.innerWidth) )  / 2)) + "px"},0)`;
+        pegatodosslides[z].style.transform=`translate(${(((document.getElementById("frontslide").scrollLeft - (indicefixo * window.innerWidth) )  / 2.5)) + "px"},0)`;
 
     }
 
