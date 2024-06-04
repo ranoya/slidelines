a30455e5b600de0464c7509ee59e85d4b615e72a
Guilherme Ranoya <ranoya@gmail.com>
2024-06-04 20:06:31 -0300

    melhorias de css

--- timelinev/component.js
+++ timelinev/component.js
@@ -26,9 +26,9 @@ timelinev = function (oldarr, ano, titulo, conteudo) {
   let quantostitulos = 1;
 
   html += `
-    <div class="timelinev_ano_head">${ano}</div>
-    <div class="timelinev_topic_head">${titulo}</div>
-    <div class="timelinev_cont_head">${conteudo}</div>
+    <div class="timelinev_ano_head"><span class="timelinev_ano_head_cont">${ano}</span></div>
+    <div class="timelinev_topic_head"><span class="timelinev_topic_head_cont">${titulo}</span></div>
+    <div class="timelinev_cont_head"><span class="timelinev_cont_head_cont">${conteudo}</span></div>
     `;
 
   for (let k = 0; k < arr.length; k++) {
@@ -54,18 +54,6 @@ timelinev = function (oldarr, ano, titulo, conteudo) {
 
     for (let s = k; s < arr.length; s++) {
       if (s < arr.length - 1) {
-        console.log(
-          s +
-            "/" +
-            (s + 1) +
-            " " +
-            tituloatual +
-            " / " +
-            arr[s + 1][titulo] +
-            " - " +
-            (arr[s + 1][titulo] == tituloatual)
-        );
-
         if (arr[s + 1][titulo] == tituloatual) {
           quantostitulos++;
         }
@@ -73,12 +61,12 @@ timelinev = function (oldarr, ano, titulo, conteudo) {
     }
 
     if (tituloatual != qualtitulo) {
-      html += `<div class="timelinev_topic" style="grid-row: span ${quantostitulos}">${arr[k][titulo]}</div>`;
+      html += `<div class="timelinev_topic" style="grid-row: span ${quantostitulos}"><span class="timelinev_topic_cont">${arr[k][titulo]}</span></div>`;
     }
 
     qualtitulo = tituloatual;
 
-    html += `<div class="timelinev_cont">${arr[k][conteudo]}</div>`;
+    html += `<div class="timelinev_cont"><span class="timelinev_cont_cont">${arr[k][conteudo]}</span></div>`;
   }
 
   html += "</div>";
