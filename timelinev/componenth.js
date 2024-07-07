e7e1f667bf0ccdb6193ecfdd258f01daacad9e47
Guilherme Ranoya <ranoya@gmail.com>
2024-07-07 19:39:39 -0300

    .0004 de precisao

--- timelinev/componenth.js
+++ timelinev/componenth.js
@@ -17,7 +17,7 @@ let timelinemovehandler = function (who, arr) {
 
   let tamanhoslideindividual = parseFloat(
     (document.querySelector(who + " .timelineh").scrollWidth +
-      document.querySelector(who + " .timelineh").scrollWidth * 0.00022) /
+      document.querySelector(who + " .timelineh").scrollWidth * 0.0004) /
       arr.length
   );
 
